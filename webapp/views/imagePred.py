07d4038b79b40d0839e77969815d40fd9ebec4c6
Adrian Lam <adrianlamcw@gmail.com>
2019-09-15 01:01:28 -0400

    Revert "updateImagePred"
    This reverts commit 10df30e7741699103e01de97b1b5f37f34562eef.

--- webapp/views/imagePred.py
+++ webapp/views/imagePred.py
@@ -5,11 +5,11 @@ def image_pred(image):
 
     prediction = CustomImagePrediction()
     prediction.setModelTypeAsInceptionV3()
-    prediction.setModelPath(os.path.join(execution_path, "model_ex-020_acc-0.992188.h5"))
+    prediction.setModelPath(os.path.join(execution_path, "model_ex-004_acc-0.492188.h5"))
     prediction.setJsonPath(os.path.join(execution_path, "model_class.json"))
     prediction.loadModel(num_objects=2)
 
-    predictions, probabilities = prediction.predictImage(os.path.join(execution_path, "up4.jpg"), result_count=2)
+    predictions, probabilities = prediction.predictImage(os.path.join(execution_path, "up.jpg"), result_count=2)
     for eachPrediction, eachProbability in zip(predictions, probabilities):
         print(eachPrediction , " : " , eachProbability)
     if eachProbability[0] > execution_path[1]:
